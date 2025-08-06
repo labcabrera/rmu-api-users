153bf2f0fb2cb819fc3b702862eeb90677777c13
labcabrera <lab.cabrera@gmail.com>
2025-08-06 15:23:32 +0200

    Added flag to disable kafka during the development.

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('User API').setDescription('API para gestión de usuarios').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, config);
+  const openApiConfig = new DocumentBuilder().setTitle('User API').setDescription('API para gestión de usuarios').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, openApiConfig);
   SwaggerModule.setup('api-docs', app, document);
 
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.KAFKA,
-  //   options: {
-  //     client: {
-  //       clientId: 'user-service',
-  //       brokers: ['localhost:9092'],
-  //     },
-  //     consumer: {
-  //       groupId: 'user-consumer',
-  //     },
-  //   },
-  // });
+  const kafkaDisabled = app.get(ConfigService).get<boolean>('DEV_KAFKA_DISABLED');
+  if (!kafkaDisabled) {
+    const clientId = app.get(ConfigService).get<string>('KAFKA_CLIENT_ID') || 'rmu-api-users';
+    const brokers = app.get(ConfigService).get<string>('KAFKA_BROKERS')?.split(',') || ['localhost:9092'];
+    const consumerGroupId = app.get(ConfigService).get<string>('KAFKA_CONSUMER_GROUP_ID') || 'user-consumer';
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: clientId,
+          brokers: brokers,
+        },
+        consumer: {
+          groupId: consumerGroupId,
+        },
+      },
+    });
+  }
 
   await app.startAllMicroservices();
   await app.listen(3010);
